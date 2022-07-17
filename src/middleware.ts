fbea68540c6938e824a78a755767066f8e426e4b
briannelson95 <71141646+briannelson95@users.noreply.github.com>
2022-07-17 18:05:45 -0400

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,11 +9,15 @@ export async function middleware(req: NextRequest, ev: NextFetchEvent) {
     }
     const slug = req.nextUrl.pathname.split("/").pop();
 
+    // console.log(req.nextUrl)
     const slugFetch = await fetch(`${req.nextUrl.origin}/api/get-url/${slug}`);
     if (slugFetch.status === 404) {
         return NextResponse.redirect(req.nextUrl.origin);
     }
+
     const data = await slugFetch.json();
+
+    // console.log(data)
 
     if (data?.url) {
         return NextResponse.redirect(data.url);
